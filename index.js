3f353656aaec74690770e530d6f11cdc26ee2ea4
Aniruddha Ghosh <aghosh0605@gmail.com>
2023-06-07 06:57:48 +0530

    🚀 Added All interface access

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ fastify.register(register);
  */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
